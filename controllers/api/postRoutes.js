27ddc47161542dd6f35ee9348c0635d8744a18cd
DylanCaff <dylancaff0902@gmail.com>
2023-04-21 16:52:35 -0400

    finished up api folder

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,7 +16,41 @@ router.post("/", withAuth, (req, res) => {
 
 router.put('/:id', withAuth, (req, res) => {
     console.log(req.body, req.params.id)
-    Post.update(req.body)
+    Post.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(affectedRows => {
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.delete("/:id", withAuth, (req, res) => {
+  console.log(req.body, req.params.id)
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(affectedRows => {
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 })
 
 
