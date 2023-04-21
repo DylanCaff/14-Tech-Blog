25b806bb979e8ee01846827301866bac88092d27
DylanCaff <dylancaff0902@gmail.com>
2023-04-21 17:56:13 -0400

    working on js for handlebars

--- public/css/js/delete.js
+++ public/css/js/delete.js
@@ -1,3 +1,15 @@
 const deleteFormHandler = async function(event) {
-    console.log()
+    console.log("clicked", event)
+    event.preventDefault();
+    const postId = document.getElementById('post-id')
+
+    fetch("/api/post/" + postId.value, {
+        method: "delete"
+    })
+    .then(function() {
+        document.location.replace("/dashborad");
+    })
+    .catch(err => console.log(err))
 }
+
+document.querySelector('#delete_btn').addEventListener("click", deleteFormHandler);
---
+++ public/css/js/new.js
@@ -0,0 +1,6 @@
+const newFormHandler = async function(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[')
+}
