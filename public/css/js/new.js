685008565ccae9bba84ec223109a4190b1f8aedf
DylanCaff <dylancaff0902@gmail.com>
2023-04-25 11:00:44 -0400

    added edit js

--- public/css/js/new.js
+++ public/css/js/new.js
@@ -3,4 +3,23 @@ const newFormHandler = async function(event) {
 
     const title = document.querySelector('input[name="post-title"]').value;
     const body = document.querySelector('textarea[')
-}
+
+    const token = localStorage.getItem("token");
+    await fetch(`/api/post`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        body
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${token}`
+      }
+    });
+  
+    document.location.replace("/dashboard");
+};
+
+document
+.querySelector("#new-post-form")
+.addEventListener("submit", newFormHandler);
